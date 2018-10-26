da3587fdab07c806d90da85c417150ca17378c0c
dudarmykola <dudar.mykola.stepan@gmail.com>
2018-10-26 02:34:14 +0200

    left-bar and support buttons, footer added

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
-console.log('js loaded');
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('menu-toggle').addEventListener('click', function () {
+    this.classList.toggle('header-bar__menu-toggle--open');
+    document.getElementById('left-bar').classList.toggle('left-bar--collapsed');
+  });
+
+  document.getElementById('support-toggle').addEventListener('click', () => {
+    document.getElementById('help').classList.toggle('help--collapsed');
+  });
+});
